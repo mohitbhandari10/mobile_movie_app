2535c764d546c8e8600444664ed88495f400b40c
mohitbhandari10 <isavitar10@gmail.com>
2025-04-21 09:30:18 +0530

    playing with routing

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
@@ -5,6 +6,8 @@ export default function Index() {
     <View className="flex-1 justify-center items-center"
     >
       <Text className="text-5xl text-dark-200">Movie screen.</Text>
+      <Link href="/onboarding">Onboarding</Link>
+      <Link href="/movie/avenger555s">Avengers Movie</Link>
     </View>
   );
 }
