4fe3f2c5b8ab9a48901ea771da8c361ea258a8de
mohitbhandari10 <isavitar10@gmail.com>
2025-04-27 00:04:12 +0530

    Revert "TMDB api"
    This reverts commit 6d83c74b1798ae2fc00849b8e61504b66a086420.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,6 @@
 import { Stack } from "expo-router";
-import "./global.css";
+import './global.css';
 
 export default function RootLayout() {
-  return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-
-      <Stack.Screen name="movie/[id]" options={{ headerShown: false }} />
-    </Stack>
-  );
+  return <Stack />;
 }
